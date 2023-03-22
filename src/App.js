6042873b489a45a9ba7b153738d6fac61dbe9ebc
w1820984-NipunLuca <w1820984@my.westminster.ac.uk>
2023-03-22 22:39:22 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import {
+  Fieldset,
+  Input,
+  Button,
+  Footer,
+  FormGroup,
+  Link,
+  Page
+} from "govuk-react";
 
-function App() {
+const RegistrationPage = () => {
+  const [formValues, setFormValues] = useState({
+    nhsNumber: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // do something with the form values, e.g. submit to server
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <body>
+      <Page>
 
-export default App;
+      <FormGroup onSubmit={handleSubmit}>
+      <p>Your NHS number is a 11 digit number, like 94627888551.</p>
+      <p>If you don’t know your NHS number you can select the link below to alternatively register using personal information</p>
+
+        <Fieldset>
+          <b>Enter NHS number</b>
+        
+            <Input
+              label="Enter NHS number"
+              name="nhsNumber"
+              value={formValues.nhsNumber}
+              onChange={handleInputChange}
+              required
+            />
+      
+            <Button>Submit</Button>
+          
+        </Fieldset>
+      </FormGroup>
+      </Page>
+
+      <div class="footer">
+        <Footer>
+            <Link href="#">Terms and conditions </Link>
+            <Link href="#">Contact us </Link>
+        </Footer>
+      </div>
+    </body>
+  );
+};
+
+export default RegistrationPage;
